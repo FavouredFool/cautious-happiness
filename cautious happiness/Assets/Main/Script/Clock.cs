0ae2bea71e762208f4636c50f55e9116896c9c23
Tim Wahrburg <t.wahrburg@gmail.com>
2023-07-09 13:45:08 +0200

    die Uhr ist uhrig

---
+++ cautious happiness/Assets/Main/Script/Clock.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hourHand;
+
+    public Transform minuteHand;
+
+    public float t = 0;
+
+    public float speed = 5;
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            t -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            t += speed * Time.deltaTime;
+        }
+
+        TToHandMovement(t);
+    }
+
+    public void TToHandMovement(float t)
+    {
+        hourHand.transform.rotation = Quaternion.Euler(0, 0, 360 * (1-t));
+
+        float tNew = t * 12;
+        minuteHand.transform.rotation = Quaternion.Euler(0, 0, 360 * (1-tNew));
+    }
+}
